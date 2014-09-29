5e2377583c6a5c6c18a3f6c00a5ff5afb26ce5f8
Aurojit <apanda@cs.berkeley.edu>
2014-09-29 14:04:48 -0700

    Get year headings
    Also figure out how to do other things

--- lib/jekyll/scholar/tags/bibliography.rb
+++ lib/jekyll/scholar/tags/bibliography.rb
@@ -31,20 +31,37 @@ module Jekyll
 
         items = items.take(max.to_i) if limit_entries?
 
-        bibliography = items.each_with_index.map { |entry, index|
-          reference = bibliography_tag(entry, index + 1)
-          puts entry['year']
-          puts reference
+        #bibliography = items.each_with_index.map { |entry, index|
+          #reference = bibliography_tag(entry, index + 1)
+          ##puts entry['year']
+          ##puts reference
+          #if generate_details?
+            #reference << link_to(details_link_for(entry),
+              #config['details_link'], :class => config['details_link_class'])
+          #end
 
+          ##content_tag :li, reference
+        #}#.join("\n")
+
+        bib_by_year = {}
+        items.each_with_index.each { |entry, index|
+          reference = bibliography_tag(entry, index + 1)
           if generate_details?
             reference << link_to(details_link_for(entry),
               config['details_link'], :class => config['details_link_class'])
           end
+          ref = content_tag :li, reference
+          key = entry['year'].to_i
+          bib_by_year[key] = bib_by_year.has_key?(key) ? bib_by_year[key].push(ref) : Array.new.push(ref)
+        }
 
-          content_tag :li, reference
+        bib_by_year.keys.sort{|a, b| a < b ? 1 : -1 }.map {|k|
+            list_of_li = bib_by_year[k].join("\n")
+
+            content_tag(:h2, k.to_s) + 
+            content_tag(:ul, list_of_li,:class => config['bibliography_class'])
         }.join("\n")
 
-        content_tag :ul, bibliography, :class => config['bibliography_class']
       end
     end
 
